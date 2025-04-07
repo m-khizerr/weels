9c1f510a71bb896ae9543b5ec2a20e9f36c3c847
m-khizerr <i200773@nu.edu.pk>
2025-04-07 19:52:20 +0500

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen">
+      <iframe
+        src="https://micarbuddy.com"
+        title="MicarBuddy WordPress Site"
+        className="w-full h-full border-0"
+      ></iframe>
+    </div>
+  );
+}
